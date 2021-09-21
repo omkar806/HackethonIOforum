399540f483e015fbd27e6cba2ccbbb2a1d792a1a
Omkar Malpure <malpureomkar5@gmail.com>
2021-09-21 19:41:54 +0530

    omkars commit

--- Hackethon/Hackethon/settings.py
+++ Hackethon/Hackethon/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2kgl)$7m+1s=w-f4wt74!k*ntbs4&npm_1eq7_@2fip-p6e*i3'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
